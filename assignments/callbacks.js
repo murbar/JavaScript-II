4a02fed18c223cf6ac162ce8b9919ca47289f66c
Joel Bartlett <joel@murbar.com>
2019-01-22 15:40:04 -0500

    Complete callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -26,23 +26,42 @@ function getLength(arr, cb) {
   // getLength passes the length of the array into the callback
   return cb(arr.length);
 }
+getLength(items, function(length) {
+  console.log(length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+last(items, function(last) {
+  console.log(last);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+sumNums(4, 5, function(sum) {
+  console.log(sum);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+multiplyNums(4, 5, function(product) {
+  console.log(product);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item));
 }
+contains('Pencil', items, function(bool) {
+  console.log(bool);
+})
 
 /* STRETCH PROBLEM */
 
