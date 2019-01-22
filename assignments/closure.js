0270612c0396671e22d9cf107b454e2f49e5eaa6
Joel Bartlett <joel@murbar.com>
2019-01-22 16:43:56 -0500

    Complete closures

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,33 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function outer() {
+  const enclosedVar = 'This is';
+
+  function inner() {
+    return `${enclosedVar} a closure.`;
+  }
+
+  return inner();
+}
+console.log(outer());
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {
+    return count += 1;
+  };
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
